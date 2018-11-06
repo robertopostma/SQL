6b67e468f5b73ed4d8b3f174e1acbb6fcc258f13
robertopostma <robertopostma@users.noreply.github.com>
2018-11-06 05:53:01 +0100

    Update test.sql
    updated to non compliant coding

--- test.sql
+++ test.sql
@@ -1,1 +1,10 @@
-select * FROM myTable where 1=1 and 2=1
+--non compliant example (missing PK)
+CREATE TABLE employee
+(
+  employee_id INTEGER NOT NULL,
+  first_name VARCHAR(42) NOT NULL,
+  last_name VARCHAR(42) NOT NULL
+);
+
+--select from mthis table 
+select * FROM myTable where nonExistingCol=1;
